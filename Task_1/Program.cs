927af076d1766d058def8558af3e319db209cd5f
DenniSergeich <damned_wolf@mail.ru>
2023-02-14 09:25:24 +0300

    Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.Write("Введите массив чисел через пробел: ");
+string messege = Console.ReadLine()!;
+string[] arrayString = messege.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[] arrayInt = GetIntArray(arrayString);
+PrintResult();
+int[] GetIntArray(string[] array)
+{
+    int[] intArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        intArray[i] = int.Parse(array[i]);
+    }
+    return intArray;
+}
+int GetResult(int[] array)
+{
+    int count = 0;
+    foreach (var item in array)
+    {
+        if (item > 0) count++;
+    }
+    return count;
+}
+void PrintResult()
+{
+    Console.WriteLine($"Пользователь ввел {GetResult(arrayInt)} числа(чисел) больше 0.");
+}
